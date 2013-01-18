3cfe975cff4cb0faac246808f47a283c2e39b1da
Keraunos <gaetan.girin.eu@gmail.com>
2013-01-18 08:57:38 +0100

    Initial Commit

---
+++ src/KMeans.java
@@ -0,0 +1,16 @@
+/**
+ * Class KMeans: contains a k-means algorithms and associated methods
+ * 
+ * @author Paulo Costa Fertonani, Gaetan Girin
+ */
+public class KMeans {
+    
+    
+    
+    public static void main(String[] args) {
+        
+        // TODO k-means algorithm
+        
+    }
+    
+}
