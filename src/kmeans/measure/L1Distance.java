e9dcd0eb515796d23d7a938599665f45d704d679
Keraunos <gaetan.girin.eu@gmail.com>
2013-01-31 18:19:31 +0100

    Implemented L1 distance

--- src/kmeans/measure/L1Distance.java
+++ src/kmeans/measure/L1Distance.java
@@ -11,14 +11,16 @@ public class L1Distance implements Measure {
     
     @Override
     public double d(Point p1, Point p2) {
-        // TODO code the distance
-        return new Double(1);
+        double res = 0;
+        for (int i = 0; i < p1.getDim(); ++i) {
+            res += Math.abs( p1.getCoord(i) - p2.getCoord(i) );
+        }
+        return res / (double) p1.getDim();
     }
     
     @Override
     public double s(Point p1, Point p2) {
-        // TODO code the similarity
-        return new Double(1);
+        return 1d / ( 1d + d(p1, p2) );
     }
     
 }
