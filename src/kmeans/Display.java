43c500cdeb843e55fcf18b7be4655c3f2939d71b
Keraunos <gaetan.girin.eu@gmail.com>
2013-01-31 18:42:39 +0100

    Commented and corrected Display class

--- src/kmeans/Display.java
+++ src/kmeans/Display.java
@@ -13,9 +13,15 @@ import javax.swing.JFrame;
  */
 public class Display extends JFrame {
     
+    /**
+     * List of graphical objects to be displayed
+     */
     private ArrayList<GraphicObject> graphicObjects;
     
     
+    /**
+     * Constructor
+     */
     public Display() {
         super("K-means");
         graphicObjects = new ArrayList<GraphicObject>();
@@ -25,15 +31,30 @@ public class Display extends JFrame {
     }
     
     
+    /**
+     * Adds a graphical object to the list of objects to be displayed
+     * 
+     * @param o The graphical object to be added
+     */
     public void addObject(GraphicObject o) {
         this.graphicObjects.add(o);
     }
     
+    /**
+     * Removes a graphical object from the list of objects to be displayed
+     * 
+     * @param o The graphical object to be removed
+     */
     public void removeObject(GraphicObject o) {
         this.graphicObjects.remove(o);
     }
     
     
+    /**
+     * Paints the frame by painting all graphical objects from the list
+     * 
+     * @param g The graphic context
+     */
     @Override
     public void paint(Graphics g) {
         
@@ -44,9 +65,6 @@ public class Display extends JFrame {
         }
         
     }
-
-    void plot(Integer[][] data) {
-        // TODO code
-    }
+    
     
 }
