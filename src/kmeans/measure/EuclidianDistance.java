0eb55fa97ac345aadc0a67cdbcf04c38f66d1b5a
Keraunos <gaetan.girin.eu@gmail.com>
2013-01-31 18:02:31 +0100

    Implemented Euclidian distance

--- src/kmeans/measure/EuclidianDistance.java
+++ src/kmeans/measure/EuclidianDistance.java
@@ -11,14 +11,16 @@ public class EuclidianDistance implements Measure {
     
     @Override
     public double d(Point p1, Point p2) {
-        // TODO code the distance
-        return new Double(0);
+        double res = 0;
+        for (int i = 0; i < p1.getDim(); ++i) {
+            res += Math.pow( p1.getCoord(i) - p2.getCoord(i) , 2);
+        }
+        return Math.sqrt(res);
     }
     
     @Override
     public double s(Point p1, Point p2) {
-        // TODO code the similarity
-        return new Double(0);
+        return 1d / ( 1d + d(p1, p2) );
     }
     
 }
